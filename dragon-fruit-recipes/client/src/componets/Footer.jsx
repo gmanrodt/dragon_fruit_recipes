2c1ef6d2a985c9fc205a62d10ea72e6aa273bc84
gwiyomiamy <octipuss@gmail.com>
2024-06-03 15:25:29 -0700

    added header, footer, and login-sign up page

---
+++ dragon-fruit-recipes/client/src/componets/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Footer() {
+
+  return (
+    <div>
+      <p className="read-the-docs">
+        <a href="https://github.com/AnnaManrodt/dragon_fruit_recipes">
+          <span className="footer-link">GitHub </span>
+        </a>         
+      </p>
+      <p>
+         Created by: Ozge, Kyle, Amy, Anna, and Tyler
+      </p>
+    </div>
+  );
+}

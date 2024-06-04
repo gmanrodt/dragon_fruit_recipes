3a8c4b73bb535a2bedbc7b7f21c08413d4a8ee36
kyle butcher <kylembutcher97@gmail.com>
2024-06-04 12:35:07 -0500

    almost done with user model. also, changed the name from user-model to user

--- dragon-fruit-recipes/server/models/User.js
+++ dragon-fruit-recipes/server/models/User.js
@@ -1,4 +1,4 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
@@ -6,6 +6,6 @@ const userSchema = new Schema(
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    
+
   }
 )

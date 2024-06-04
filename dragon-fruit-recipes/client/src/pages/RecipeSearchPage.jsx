fb19e7da0309bb88ae8eb57738c3c4147eaceaea
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-04 12:42:22 -0500

    got the form done

--- dragon-fruit-recipes/client/src/pages/RecipeSearchPage.jsx
+++ dragon-fruit-recipes/client/src/pages/RecipeSearchPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useLocation } from "react";
-import { NavLink  } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
+import { Button, Form, FormGroup, FormLabel, FormControl } from 'react-bootstrap'
 export default function RecipeSearch() {
 
     const navigate = useNavigate();
@@ -10,8 +10,33 @@ export default function RecipeSearch() {
         navigate('/search');
     }, []);
 
-    return(
+    return (
         <>
+            <Form className="border p-3">
+                <FormGroup className="mb-3">
+                    <FormLabel>Search by Title</FormLabel>
+                    <FormControl type="text" placeholder="Search by title" />
+                </FormGroup>
+                <FormGroup className="mb-3">
+                    <FormLabel>Search by Category</FormLabel>
+                    <select id="dropdown-basic-button" title="Select Category">
+                        <option value="beef">Beef</option>
+                        <option value="volvo">Chicken</option>
+                        <option value="volvo">Dessert</option>
+                        <option value="volvo">Lamb</option>
+                        <option value="volvo">Miscellaneous</option>
+                        <option value="volvo">Pork</option>
+                        <option value="volvo">Seafood</option>
+                        <option value="volvo">Side</option>
+                        <option value="volvo">Starter</option>
+                        <option value="volvo">Vegan</option>
+                        <option value="volvo">Vegetarian</option>
+                        <option value="volvo">Breakfast</option>
+                        <option value="volvo">Goat</option>
+                    </select >
+                </FormGroup>
+                <Button type="submit" variant="primary">Submit</Button>
+            </Form>
         </>
     )
 

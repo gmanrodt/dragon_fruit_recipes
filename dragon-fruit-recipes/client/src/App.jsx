c37c85969f51c1fa653eca2a9748d6bc5c9d2cd3
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-04 11:00:02 -0500

    finsihed nested routes

--- dragon-fruit-recipes/client/src/App.jsx
+++ dragon-fruit-recipes/client/src/App.jsx
@@ -8,8 +8,8 @@ import LoginSignup from './pages/LoginSignupPage';
 import RecipeSearch from './pages/RecipeSearchPage';
 import SingleRecipe from './pages/SingleRecipePage';
 import CreateRecipe from './pages/CreateRecipe';
-import UserInfoPage from './pages/UserInfoPage';
 import UserInfo from './pages/UserPage';
+import UserInfoPage from './pages/UserInfoPage';
 import ViewCreatedRecipe from './pages/ViewCreatedRecipe';
 import VeiwSavedRecipe from './pages/ViewSavedRecipe';
 
@@ -22,19 +22,19 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginSignup />} />
           <Route path="/search" element={<RecipeSearch />} />
           <Route path="/recipe" element={<SingleRecipe />} />
           <Route path="/create" element={<CreateRecipe />} />
-          <Route path="/userinfo" element={<UserInfoPage />} />
           <Route path="/user" element={<UserInfo />} />
-          <Route path="/created" element={<ViewCreatedRecipe />} />
-          <Route path="/saved" element={<VeiwSavedRecipe />} />
+          <Route path="user/userinfo" element={<UserInfoPage />} />
+          <Route path="user/created" element={<ViewCreatedRecipe />} />
+          <Route path="user/saved" element={<VeiwSavedRecipe />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
 
     </>
--- dragon-fruit-recipes/client/src/pages/ViewCreatedRecipe.jsx
+++ dragon-fruit-recipes/client/src/pages/ViewCreatedRecipe.jsx
@@ -2,16 +2,11 @@ import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 export default function ViewCreatedRecipe() {
-    
-    const navigate = useNavigate();
-    useEffect(() => {
-        // Call navigate() inside the useEffect hook
-        navigate('/created');
-    }, []);
+
 
     return(
         <>
-       </>
+    </>
     )
 
 }

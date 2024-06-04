f4f1221ed61cb4243b287d3440d031db76dd1a42
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-04 11:33:41 -0500

    set up logic for both veiw pages

--- dragon-fruit-recipes/client/src/pages/ViewCreatedRecipe.jsx
+++ dragon-fruit-recipes/client/src/pages/ViewCreatedRecipe.jsx
@@ -1,12 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 export default function ViewCreatedRecipe() {
 
+    const [recipes, setRecipes] = useState;
+    useEffect(() => {
+        const fetchCreatedRecipes = async () => {
+            try {
+                const response = await axios.get('this is where our database link will go'); //fixed this 
+                setRecipes(response.data)
+            }
+            catch (error) {
+                console.log('error fetching recipes', error)
+            }
+        };
+        fetchCreatedRecipes();
+    }, [])
 
-    return(
+    return (
         <>
-    </>
+            <h3>Created Recipes</h3>
+            <ul>
+            {/* this will need to be redone also once we know how the back end works */}
+                {recipes.map(recipes => (
+                    <li key={recipes._id}>{recipes.title}</li>
+                ))}
+            </ul>
+        </>
     )
 
 }
--- dragon-fruit-recipes/client/src/pages/ViewSavedRecipe.jsx
+++ dragon-fruit-recipes/client/src/pages/ViewSavedRecipe.jsx
@@ -1,18 +1,33 @@
 import React, { useState, useEffect, useLocation } from "react";
 import { NavLink  } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-export default function VeiwSavedRecipe() {
+export default function ViewSavedRecipe() {
 
-    // const navigate = useNavigate();
-    // useEffect(() => {
-    //     // Call navigate() inside the useEffect hook
-    //     navigate('/saved');
-    // }, []);
+    const [recipes, setRecipes] = useState;
+    useEffect(() => {
+        const fetchSavedRecipes = async () => {
+            try {
+                const response = await axios.get('this is where our database link will go'); //fixed this 
+                setRecipes(response.data)
+            }
+            catch (error) {
+                console.log('error fetching recipes', error)
+            }
+        };
+        fetchSavedRecipes();
+    }, [])
 
-    return(
-       <>
-       </>
+    return (
+        <>
+            <h3>Saved Recipes</h3>
+            <ul>
+                {/* this will need to be redone also once we know how the back end works */}
+                {recipes.map(recipes => (
+                    <li key={recipes._id}>{recipes.title}</li>
+                ))}
+            </ul>
+        </>
     )
 
 }

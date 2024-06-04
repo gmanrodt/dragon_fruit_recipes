064ff9ccac5032ebc18184cd628c627ee7fc7c3a
TyWalter <walterty89@gmail.com>
2024-06-04 11:36:30 -0500

    Saving data before another merge

--- dragon-fruit-recipes/server/controllers/user-controllers.js
+++ dragon-fruit-recipes/server/controllers/user-controllers.js
@@ -18,7 +18,7 @@ module.exports = {
       const user = await User.find();
       res.status(200).json(user);
     } catch(err) {
-      res.status(500).json(err.message);
+      res.status(500).json({msg: "Get users: " + err.message});
     };
   },
 
@@ -28,10 +28,10 @@ module.exports = {
       const user = await User.findOne({_id: req.params.userId});
       if(!user) {
         res.status(404).json({msg: "No user found with that ID"});
-      }
+      };
       res.status(200).json(user);
     } catch(err) {
-      res.status(500).json(err.message);
+      res.status(500).json({msg: "Get users: " + err.message});
     };
   },
 
@@ -41,7 +41,34 @@ module.exports = {
       const user = await User.create(req.body);
       const token = await createToken(user);
       res
-        .status
-    }
-  }
+        .status(200)
+        .cookie("auth-cookie", token, {
+          maxAge: 86400 * 1000, // 24 hour token
+          httpOnly: false,
+          secure: process.env.NODE_ENV === "production"
+        })
+        .json(user);
+    } catch(err) {
+      res.status(500).json({msg: "Create user: " + err.message});
+    };
+  },
+
+  // Update user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$set: req.body},
+        {runValidators: true, new: true}
+      );
+      if(!user) {
+        res.status(404).json({msg: "No user found with that ID"});
+      };
+      res.status(200).json(user);
+    } catch(err) {
+      res.status(500).json({msg: "Update user" + err.message});
+    };
+  },
+
+  
 }

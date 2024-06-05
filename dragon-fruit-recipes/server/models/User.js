2e1ebf94e71dfe1bc4b4a0c85e984a4f489e3186
kyle butcher <kylembutcher97@gmail.com>
2024-06-04 22:09:29 -0500

    finished the user model.

--- dragon-fruit-recipes/server/models/User.js
+++ dragon-fruit-recipes/server/models/User.js
@@ -1,39 +1,42 @@
-// const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new Schema(
-//   {
-//     userId: {
-//       type: Schema.Types.ObjectId,
-//       default: () => new Types.ObjectId(),
-//     },
-//     username: {
-//       type: String,
-//       required: true,
-//       max_length: 50,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       max_length: 50,
-//     },
-//     savedRecipe: {
-//       type: 
-//     // },
-//     reviews: {
-//       type: String,
-//       required: true,
-//       max_length: 50,
-//     },
-//     createdRecipes: {
-//       type: String,
-//       required: true,
-//       max_length: 50,
-//     },
-//   {
-//     toJSON: {
-//       getters: true,
-//     }
-//   }
 
-//   }
-// )
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    savedRecipe: {
+      type: String,
+    },
+    reviews: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    createdRecipes: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    }
+  }
+);
+
+const User = new mongoose.model("user", userSchema);
+module.exports = User;

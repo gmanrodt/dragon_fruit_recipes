7be9b92f9834e97f7cf0c72dec7f094bca14b894
TyWalter <walterty89@gmail.com>
2024-06-07 16:15:30 -0500

    Path join

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "..", 'client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve( __dirname, "..", 'client/dist/index.html'))
+        res.sendFile(path.join( __dirname, '../client/dist/index.html'))
     })
 }
 

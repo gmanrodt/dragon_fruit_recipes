c83ea4241645aecffebfd0695190c4ee3b4bb960
TyWalter <walterty89@gmail.com>
2024-06-07 11:46:28 -0500

    Nothing to commit here

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,31 +1,30 @@
+// Requiring in connection, model, and database
 const connection = require("../config/connection");
 const Recipe = require("../models/Recipe");
 const recipeData = require("./recipes.json");
 
+// Starting new connection
 connection.once('open', async() => {
-
-  const recipesExists = await connection.db.listCollections({ name: "recipes" }).toArray()
+  // Clearing collections
+  const recipesExists = await connection.db.listCollections({ name: "recipes" }).toArray();
   if( recipesExists.length ){
-    await connection.db.dropCollection("recipes")
+    await connection.db.dropCollection("recipes");
   }
-  
-  const reviewsExists = await connection.db.listCollections({ name: "reviews" }).toArray()
+  const reviewsExists = await connection.db.listCollections({ name: "reviews" }).toArray();
   if( reviewsExists.length ){
-    await connection.db.dropCollection("reviews")
+    await connection.db.dropCollection("reviews");
+  }
+  const usersExists = await connection.db.listCollections({ name: "users" }).toArray();
+  if( usersExists.length ){
+    await connection.db.dropCollection("users");
   }
 
+  // Creating new collection
   try {
-    await Product.insertMany(dizzydata)
-    console.log("Product seeding successful")
+    await Recipe.insertMany(recipeData)
+    console.log("Recipe seeding successful")
   } catch(err){
-    console.log("Product seeding failed - " + err.message)
-  }
-
-  try {
-    await User.insertMany(userdata)
-    console.log("User seeding successful")
-  } catch(err){
-    console.log("User seeding failed - " + err.message)
+    console.log("Recipe seeding failed: " + err.message)
   }
 
   process.exit(0)

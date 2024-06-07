3035fa0b7cfe511e5983ef4601a5cc8e656aff8e
TyWalter <walterty89@gmail.com>
2024-06-07 15:55:09 -0500

    connection to render

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,6 +1,8 @@
-// Requiring in mongoose
+// Requiring in mongoose and dotenv
 const {connect, connection} = require("mongoose");
-const connectionString = "mongodb://127.0.0.1:27017/dragonFruitDelightsDB";
+const path = require('path');
+require("dotenv").config({path: path.join(__dirname, "../.env")})
+const connectionString = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dragonFruitDelightsDB";
 
 // Connection
 connect(connectionString);

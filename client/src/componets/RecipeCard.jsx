1a322ec86603abc7f0a512185f1aa97048bc5697
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-07 09:44:49 -0500

    started fixing some bugs

--- client/src/componets/RecipeCard.jsx
+++ client/src/componets/RecipeCard.jsx
@@ -1,16 +1,16 @@
 // TO DO: import API content
 
 // TO DO: check that these match what we want to display of the API
-// function RecipeCard({props}){
+function RecipeCard({props}){
 
-//    return(
-//       <div className="card">
-//          <h2>{props?.name}</h2>
-//          <h4>Ingredients</h4>
-//          <p>{props?.measurements}{props?.ingredients}</p>
-//          <h4>Instructions</h4>
-//          <p>{props?.instructions}</p>
-//       </div>
-//    )
-// }
-// export default RecipeCard;
+   return(
+      <div className="card">
+         <h2>{props?.name}</h2>
+         <h4>Ingredients</h4>
+         <p>{props?.measurements}{props?.ingredients}</p>
+         <h4>Instructions</h4>
+         <p>{props?.instructions}</p>
+      </div>
+   )
+}
+export default RecipeCard;
--- client/src/pages/ViewSavedRecipe.jsx
+++ client/src/pages/ViewSavedRecipe.jsx
@@ -7,47 +7,47 @@ import '../style/viewsaved.css'
 
 export default function ViewSavedRecipe() {
 
-    // const [recipes, setRecipes] = useState;
-    // useEffect(() => {
-    //     const fetchSavedRecipes = async () => {
-    //         try {
-    //             const response = await axios.get('this is where our database link will go'); //fixed this 
-    //             setRecipes(response.data)
-    //         }
-    //         catch (error) {
-    //             console.log('error fetching recipes', error)
-    //         }
-    //     };
-    //     fetchSavedRecipes();
-    // }, [])
+    const [recipes, setRecipes] = useState([]); // Initialize recipes as an empty array
+
+    useEffect(() => {
+        const fetchSavedRecipes = async () => {
+            try {
+                const response = await axios.get('this is where our database link will go');
+                setRecipes(response.data);
+            } catch (error) {
+                console.log('error fetching recipes', error);
+            }
+        };
+        fetchSavedRecipes();
+    }, []);
+    
 
     return (
         <>
         <UserNav/>
-            <h3>Saved Recipes</h3>
-            {/* <ul>
-                {this will need to be redone also once we know how the back end works }
-                {recipes.map(recipes => (
-                    <li key={recipes._id}>{recipes.title}</li>
-                ))}
-            </ul> */}
-               {/* <div class='recipecards'>
-          <div class='recipe1'>
+        <h3>Saved Recipes</h3>
+        <ul>
+            {recipes.map(recipe => (
+                <li key={recipe._id}>{recipe.title}</li>
+            ))}
+        </ul>
+              <div className='recipecards'>
+          <div className='recipe1'>
            <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
            <p>Title</p>
          </div>
-         <div class='recipe2'>
+         <div className='recipe2'>
            <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
            <p>Title</p>
         </div>
-        <div class='recipe3'>
+        <div className='recipe3'>
            <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
            <p>Title</p>
         </div>
         </div>
        
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a> */}
+  <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a className="next" onclick="plusSlides(1)">&#10095;</a>
             <div className="container-carousel">
                 <input type="radio" name="position" checked />
                 <input type="radio" name="position" />
@@ -55,35 +55,35 @@ export default function ViewSavedRecipe() {
                 <input type="radio" name="position" />
                 <input type="radio" name="position" />
                 <div id="carousel">
-                    <div class="item">
+                    <div className="item">
                         <div className='recipe-card'>
                             <img src="/assets/dragonfruit.png" alt="sample recipe" className="recipeImg"/>
                             <h2>Sample Food</h2>
                             <p>Description of food or something.</p>
                         </div>
                     </div>
-                    <div class="item">
+                    <div className="item">
                         <div className='recipe-card'>
                             <img src="/assets/dragonfruit.png" alt="sample recipe" className="recipeImg"/>
                             <h2>Sample Food</h2>
                             <p>Description of food or something.</p>
                         </div>
                     </div>
-                    <div class="item">
+                    <div className="item">
                         <div className='recipe-card'>
                             <img src="/assets/dragonfruit.png" alt="sample recipe" className="recipeImg"/>
                             <h2>Sample Food</h2>
                             <p>Description of food or something.</p>
                         </div>
                     </div>
-                    <div class="item">
+                    <div className="item">
                         <div className='recipe-card'>
                             <img src="/assets/dragonfruit.png" alt="sample recipe" className="recipeImg"/>
                             <h2>Sample Food</h2>
                             <p>Description of food or something.</p>
                         </div>
                     </div>
-                    <div class="item">
+                    <div className="item">
                         <div className='recipe-card'>
                         <img src="/assets/dragonfruit.png" alt="sample recipe" className="recipeImg"/>
                         <h2>Sample Food</h2>

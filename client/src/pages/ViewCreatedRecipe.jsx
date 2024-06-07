234ab6100ed032d99267e672427eed6e5198b063
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-07 09:19:05 -0500

    some changes

--- client/src/pages/ViewCreatedRecipe.jsx
+++ client/src/pages/ViewCreatedRecipe.jsx
@@ -6,46 +6,45 @@ import '../style/viewrecipes.css'
 
 export default function ViewCreatedRecipe() {
 
-   // const [recipes, setRecipes] = useState;
-  //  useEffect(() => {
-     //   const fetchCreatedRecipes = async () => {
-       //     try {
-           //     const response = await axios.get('this is where our database link will go'); //fixed this 
-          //      setRecipes(response.data)
-          //  }
-         //   catch (error) {
-        //        console.log('error fetching recipes', error)
-        //    }
-       // };
-     //   fetchCreatedRecipes();
-   // }, [])
+  const [recipes, setRecipes] = useState;
+  useEffect(() => {
+    const fetchCreatedRecipes = async () => {
+      try {
+        const response = await axios.get('api/recipe'); //fixed this 
+        setRecipes(response.data)
+      }
+      catch (error) {
+        console.log('error fetching recipes', error)
+      }
+    };
+    fetchCreatedRecipes();
+  }, [])
 
-    return (
-        <>
-        <UserNav/>
-            <h3>Created Recipes</h3>
-          {/* <ul>
-          { this will need to be redone also once we know how the back end works }
+  return (
+    <>
+      <UserNav />
+      <h3>Created Recipes</h3>
+      <ul>
                 {recipes.map(recipes => (
                  <li key={recipes._id}>{recipes.title}</li>
            ))}
-          </ul> */}
-          <div class='recipecards'>
-          <div class='recipe1'>
-           <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
-           <p>Title</p>
-         </div>
-         <div class='recipe2'>
-           <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
-           <p>Title</p>
+          </ul>
+      <div class='recipecards'>
+        <div class='recipe1'>
+          <img src="/assets/dragonfruit.png" alt="Logo" id="logo" />
+          <p>Title</p>
+        </div>
+        <div class='recipe2'>
+          <img src="/assets/dragonfruit.png" alt="Logo" id="logo" />
+          <p>Title</p>
         </div>
         <div class='recipe3'>
-           <img src="/assets/dragonfruit.png" alt="Logo" id="logo"/>
-           <p>Title</p>
+          <img src="/assets/dragonfruit.png" alt="Logo" id="logo" />
+          <p>Title</p>
         </div>
-        </div>
-            
-        </>
-    )
+      </div>
+
+    </>
+  )
 
 }

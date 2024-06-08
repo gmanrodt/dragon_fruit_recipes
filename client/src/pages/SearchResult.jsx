f33271fbb316d8be044cafd0ee901129d0adefb3
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-08 10:50:35 -0500

    got some code to search by recipe

--- client/src/pages/SearchResult.jsx
+++ client/src/pages/SearchResult.jsx
@@ -2,13 +2,31 @@ import React, { useState, useEffect, useLocation } from "react";
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { Button, Form, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
-
+import RecipeSearch from "./RecipeSearchPage";
 export default function searchResults() {
 
   async function getSearchResults() {
-    const result = await ("/api/SearchResult")
+    const result = await ("/api/recipes")
     const data = result.json()
 
+  }
+  <ChildComponent category={formData.category} />
+
+  async function getByCategory(){
+    fetch(`/api/recipes/${category}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(recipe => {
+    // Handle the retrieved recipe data
+    console.log(recipe);
+  })
+  .catch(error => {
+    console.error('Error fetching recipe:', error);
+  });
   }
 
 return (

74f8ad5915f7f3b7e099c2189f7a14aa7a598933
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-06-09 12:27:46 -0500

    got user info displaying

--- client/src/pages/UserInfoPage.jsx
+++ client/src/pages/UserInfoPage.jsx
@@ -2,45 +2,53 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../style/userInfo.css"
 import UserNav from '../componets/UserPageNav'
-
+import { useAppContext } from "../providers/AppProvider";
 
 export default function UserInfoPage() {
     const navigateTo = useNavigate();
     const [userData, setUserData] = useState({});
 
-    useEffect(() => {
-        fetch('api/user')
-            .then(response => response.json())
-            .then(data => setUserData(data));
-    }, []);
+    const {currentUser} = useAppContext()
 
-    const handleDeleteUser = (userId) => {
-        fetch(`api/users/${userId}`, {
-            method: 'DELETE',
-        })
-        .then(response => {
-            if (response.ok) {
-                // Update the UI or navigate to a different page after deletion
-                navigateTo.push('/'); // Redirect to the homepage
-            } else {
-                console.log('Error deleting user');
-            }
-        })
-        .catch(error => console.error('Error deleting user:', error));
-    };
+    useEffect(() => {
+            fetch(`api/users/${userData._id}`)
+                .then(response => response.json())
+                .then(data => setUserData(data));
+        
+    }, []);
+    console.log(currentUser)
+
+useEffect(()=>{
+currentUser && setUserData(currentUser)
+}, [currentUser])
+
+    // const handleDeleteUser = (userId) => {
+    //     fetch(`api/users/${userId}`, {
+    //         method: 'DELETE',
+    //     })
+    //     .then(response => {
+    //         if (response.ok) {
+    //             // Update the UI or navigate to a different page after deletion
+    //             navigateTo.push('/'); // Redirect to the homepage
+    //         } else {
+    //             console.log('Error deleting user');
+    //         }
+    //     })
+    //     .catch(error => console.error('Error deleting user:', error));
+    // };
 
     return (
         <>
         <UserNav/>
         <div className="amUserInfo">
-            <div>Username: {userData.userName}</div>
+            <div>Username: {userData?.username}</div>
             <br/>
-            <div>Email: {userData.email}</div>
+            <div>Email: {userData?.email}</div>
             <br/>
-            <div>You have saved {userData.savedRecipes} recipes!</div>
+            {/* <div>You have saved {userData?.savedRecipes} recipes!</div>
             <br/>
-            <div>You have created {userData.createdRecipes} recipes!</div>
-            <br/>
+            <div>You have created {userData?.createdRecipes} recipes!</div>
+            <br/> */}
             <button className="margin" onClick={() => handleDeleteUser(userData.userId)}>Delete User</button>
             <button className="margin">LogOut</button>
         </div>
